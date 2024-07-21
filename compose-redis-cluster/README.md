ac4902a065f62ca5184f663b004d580bbcf13ebe
ixueshao <945106652@qq.com>
2024-07-21 09:15:56 +0800

    新增Redis cluster一键安装

---
+++ compose-redis-cluster/README.md
@@ -0,0 +1,17 @@
+- 安装install
+
+```shell
+bash ./prepare.sh install
+```
+
+- 卸载(uninstall)
+
+```shell
+bash ./prepare.sh uninstall
+```
+
+- 集群信息检查(check)
+
+```bash
+bash ./prepare.sh check
+```
---
+++ compose-redis-cluster/prepare.sh
@@ -0,0 +1,67 @@
+#！/bin/env bash
+
+
+returnExitMsg(){
+  if [ `echo $?` -eq 0 ]; then
+    echo "$1:执行成功!"
+  else
+    echo "$1:执行失败!"
+  fi
+}
+
+printMsg(){
+  echo "$1: 开始执行..."
+}
+
+#安装集群
+install(){
+  printMsg "安装集群"
+  #安装redis
+  docker-compose up -d
+  returnExitMsg "安装集群"
+}
+
+#卸载集群
+uninstall(){
+  printMsg "卸载集群"
+  #停止集群
+  docker-compose down
+  returnExitMsg "卸载集群"
+}
+
+
+#初始化集群
+init(){
+  printMsg "初始化集群"
+  #初始化集群
+  yes yes | docker exec -i redis1 redis-cli --cluster create \
+    redis1:6379 redis2:6379 redis3:6379 redis4:6379 redis5:6379 \
+    redis6:6379 --cluster-replicas 1
+  returnExitMsg "初始化集群"
+}
+
+
+#检查集群状态
+check(){
+  printMsg "检查集群状态"
+  #检查集群状态
+  docker exec -i redis1 redis-cli cluster info
+  printMsg "检查集群槽位分布"
+  docker exec -i redis1 redis-cli CLUSTER SLOTS
+  printMsg "检查集群节点状态"
+  docker exec -i redis1 redis-cli --cluster check redis1:6379
+  returnExitMsg "检查集群状态"
+}
+
+#判断脚本的第一个参数是install还是uninstall, 为install则执行安装, 为uninstall则执行卸载
+if [ $1 = "install" ]; then
+  install
+  init
+  check
+elif [ $1 = "uninstall" ]; then
+  uninstall
+elif [ $1 = "check" ]; then
+  check
+else
+  echo "请输入正确的参数: install, uninstall, init, check"
+fi
